9d904998cdca251235c0aa4ea2efd4c4fcd8e36d
Bárbara Furquim <113610637+Barb-Furquim@users.noreply.github.com>
2023-04-24 10:21:19 -0300

    Create preco.c

---
+++ preco.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void){
+    int numero_turma;
+    float preco_gasolina;
+    numero_turma=20;
+    preco_gasolina=15.6;
+
+    printf("Número de alunos na turma: %d\n", numero_turma);
+    printf("O preço da gasolina: %f\n", preco_gasolina);
+    return 0;
+}
